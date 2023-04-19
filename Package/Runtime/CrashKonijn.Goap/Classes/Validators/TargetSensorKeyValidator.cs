b1c004c2957fc6223a762258d5583381d93e6101
Peter Klooster <crashkonijn@gmail.com>
2023-04-19 16:41:45 +0200

    Added TargetSensorKeyValidator and WorldSensorKeyValidator

---
+++ Package/Runtime/CrashKonijn.Goap/Classes/Validators/TargetSensorKeyValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using CrashKonijn.Goap.Configs.Interfaces;
+using CrashKonijn.Goap.Interfaces;
+
+namespace CrashKonijn.Goap.Classes.Validators
+{
+    public class TargetSensorKeyValidator : IValidator<IGoapSetConfig>
+    {
+        public void Validate(IGoapSetConfig config, ValidationResults results)
+        {
+            var missing = config.TargetSensors.Where(x => x.Key == null).ToArray();
+            
+            if (!missing.Any())
+                return;
+            
+            results.AddError($"TargetSensors without Key: {string.Join(", ", missing.Select(x => x.Name))}");
+        }
+    }
+}
---
+++ Package/Runtime/CrashKonijn.Goap/Classes/Validators/WorldSensorKeyValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using CrashKonijn.Goap.Configs.Interfaces;
+using CrashKonijn.Goap.Interfaces;
+
+namespace CrashKonijn.Goap.Classes.Validators
+{
+    public class WorldSensorKeyValidator : IValidator<IGoapSetConfig>
+    {
+        public void Validate(IGoapSetConfig config, ValidationResults results)
+        {
+            var missing = config.WorldSensors.Where(x => x.Key == null).ToArray();
+            
+            if (!missing.Any())
+                return;
+            
+            results.AddError($"WorldSensors without Key: {string.Join(", ", missing.Select(x => x.Name))}");
+        }
+    }
+}
