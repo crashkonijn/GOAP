d830e0164f80e3655571586ff1eaf3fa01eb818c
Peter Klooster <crashkonijn@gmail.com>
2023-05-01 09:05:07 +0200

    Fixed AgentBehaviour being in GoapSet contract

--- Package/Runtime/CrashKonijn.Goap/Interfaces/IGoapSet.cs
+++ Package/Runtime/CrashKonijn.Goap/Interfaces/IGoapSet.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using CrashKonijn.Goap.Behaviours;
 using CrashKonijn.Goap.Classes;
-using CrashKonijn.Goap.Classes.Runners;
 using CrashKonijn.Goap.Resolver.Interfaces;
 
 namespace CrashKonijn.Goap.Interfaces
@@ -12,8 +11,8 @@ namespace CrashKonijn.Goap.Interfaces
         GoapConfig GoapConfig { get; }
         IAgentCollection Agents { get; }
         ISensorRunner SensorRunner { get; }
-        void Register(AgentBehaviour agent);
-        void Unregister(AgentBehaviour agent);
+        void Register(IMonoAgent agent);
+        void Unregister(IMonoAgent agent);
         List<IAction> GetAllNodes();
         List<IActionBase> GetActions();
 
