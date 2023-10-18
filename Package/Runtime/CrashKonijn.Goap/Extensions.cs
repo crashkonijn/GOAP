59bec0e37b685243895fc70e32913303df82070d
Peter Klooster <crashkonijn@gmail.com>
2023-10-18 20:50:05 +0200

    Initial version of BehaviourSet config

--- Package/Runtime/CrashKonijn.Goap/Extensions.cs
+++ Package/Runtime/CrashKonijn.Goap/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using CrashKonijn.Goap.Interfaces;
+﻿using CrashKonijn.Goap.Enums;
+using CrashKonijn.Goap.Interfaces;
+using CrashKonijn.Goap.Resolver;
 using UnityEngine;
 
 namespace CrashKonijn.Goap
@@ -7,5 +9,39 @@ namespace CrashKonijn.Goap
     {
         public static bool IsNull(this IMonoAgent agent)
             => agent is MonoBehaviour mono && mono == null;
+
+        public static string ToName(this Comparison comparison)
+        {
+            return comparison switch
+            {
+                Comparison.SmallerThan => "<",
+                Comparison.SmallerThanOrEqual => "<=",
+                Comparison.GreaterThan => ">",
+                Comparison.GreaterThanOrEqual => ">=",
+                _ => throw new System.NotImplementedException()
+            };
+        }
+        
+        public static Comparison FromName(this string comparison)
+        {
+            return comparison switch
+            {
+                "<" => Comparison.SmallerThan,
+                "<=" => Comparison.SmallerThanOrEqual,
+                ">" => Comparison.GreaterThan,
+                ">=" => Comparison.GreaterThanOrEqual,
+                _ => throw new System.NotImplementedException()
+            };
+        }
+        
+        public static string ToName(this EffectType type)
+        {
+            return type switch
+            {
+                EffectType.Increase => "++",
+                EffectType.Decrease => "--",
+                _ => throw new System.NotImplementedException()
+            };
+        }
     }
 }
