d471ee9108fed674b131c5145d869c75fa0413be
Peter Klooster <crashkonijn@gmail.com>
2023-11-30 21:16:59 +0100

    Improved editor readability (especially in light mode)

--- Package/Editor/CrashKonijn.Goap.Editor/NodeViewer/Drawers/NodeDrawer.cs
+++ Package/Editor/CrashKonijn.Goap.Editor/NodeViewer/Drawers/NodeDrawer.cs
@@ -98,7 +98,7 @@ namespace CrashKonijn.Goap.Editor.NodeViewer.Drawers
         
         private string GetText(ICondition condition, bool value)
         {
-            var color = value ? "green" : "red";
+            var color = value ? "#1A9018" : "#DD1C1A";
 
             return $"    <color={color}>{condition.WorldKey.Name} {GetText(condition.Comparison)} {condition.Amount}</color>";
         }
