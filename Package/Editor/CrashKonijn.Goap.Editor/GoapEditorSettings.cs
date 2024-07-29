10c00206ba82ccb780d40cd383f2436d9ee033c7
crashkonijn <crashkonijn@users.noreply.github.com>
2024-07-29 11:53:27 +0000

    Update version to 3.0.9-beta

--- Package/Editor/CrashKonijn.Goap.Editor/GoapEditorSettings.cs
+++ Package/Editor/CrashKonijn.Goap.Editor/GoapEditorSettings.cs
@@ -19,6 +19,6 @@ namespace CrashKonijn.Goap.Editor
             }
         }
       
-        public const string Version = "3.0.8-beta";
+        public const string Version = "3.0.9-beta";
     }
 }
